97f58a62f0673e963616f8f0800d918be37ff4f1
Manjinder Singh <manji.sidhu@gmail.com>
2019-12-06 12:27:18 +0530

    init

---
+++ app/src/main/java/com/omninos/cacheproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.omninos.cacheproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
